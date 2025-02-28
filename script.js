5653c5079987556e6f5fec1f8176fad8c57d0063
painisreal <ariknesh05@.com>
2025-02-28 14:56:06 +0530

    chore

---
+++ script.js
@@ -0,0 +1,16 @@
+// let age = 19;
+
+
+
+// let type = age > 18 ? "Adult ticket" : "child ticket"
+// console.log(type);
+function check() {
+let age_id = document.getElementById("age");
+let value = age_id.value;
+value >=18 ? window.alert("Adult ticket") : window.alert("Child ticket");
+}
+
+
+
+
+
