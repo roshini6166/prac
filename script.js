120426e96c1d970fc359163db7e91d078361386c
painisreal <ariknesh05@.com>
2025-02-28 15:07:49 +0530

    chore

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-// let age = 19;
+// // let age = 19;
 
 
 
-// let type = age > 18 ? "Adult ticket" : "child ticket"
-// console.log(type);
-function check() {
-let age_id = document.getElementById("age");
-let value = age_id.value;
-value >=18 ? window.alert("Adult ticket") : window.alert("Child ticket");
+// // let type = age > 18 ? "Adult ticket" : "child ticket"
+// // console.log(type);
+// function check() {
+// let age_id = document.getElementById("age");
+// let value = age_id.value;
+// value >=18 ? window.alert("Adult ticket") : window.alert("Child ticket");
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+for(let i= 1; i <= 60; i++){
+    i%2 !==0 ? console.log(`Number: ${i}`) : '';
 }
-
-
-
-
-
